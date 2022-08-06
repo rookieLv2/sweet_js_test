998eb4690a9e5ec0eefaf71f6a93675b1cac68c4
rookieLv2 <boboisgodd@gmail.com>
2022-08-06 22:08:49 +0800

    add price
    新增text_value 負號bug，再用了event.target抓到element，最後用了for each把code縮成一行。

--- js/cart.js
+++ js/cart.js
@@ -1,11 +1,14 @@
 // 取得元素
-const icon_parent=document.querySelectorAll(".select")
-const price=document.querySelectorAll(".total_price")
+const icon_parent = document.querySelectorAll(".select")
+const price = document.querySelectorAll(".total_price")
+const text_number = document.querySelectorAll("input")
+const order_cash= document.querySelectorAll(".price")
 // let add_icon = icon_parent[0].children[2]
 // let minus_icon = icon_parent[0].children[0]
 
 // 新增事件
 function add_number(event){
+    let test = event.target
     let text = event.target.parentNode.children[1]
     let parsed = parseInt(text.value, 10)
     if (isNaN(parsed)) {    
@@ -18,7 +21,8 @@ function add_number(event){
         parsed = 0
     
     text.value = parsed
-    price[0].innerText = 450 * text.value
+    test.parentNode.parentNode.children[3].innerText = "NT$"+450 * text.value
+    
 }
 
 // 使用fuction寫法
@@ -33,7 +37,7 @@ for(var i=0; i<icon_parent.length; i++){
     
     // 用target 抓到被點的人(i)
     icon_parent[i].children[0].addEventListener('click', function (event) {
-        
+        let test = event.target
         let text = event.target.parentNode.children[1]
         let parsed = parseInt(text.value, 10)
         if (isNaN(parsed)) {    
@@ -46,6 +50,31 @@ for(var i=0; i<icon_parent.length; i++){
             parsed = 0
         
         text.value = parsed
-        price[0].innerText = 450 * text.value
+        test.parentNode.parentNode.children[3].innerText = "NT$"+450 * text.value
     })    
 } 
+
+function updateValue(element) {
+    element.parentNode.parentNode.children[3].innerText = "NT$"+450 * element.value
+    if(element.value < 0)
+        element.parentNode.parentNode.children[3].innerText = "NT$"+0
+}
+
+text_number.forEach(element => {
+    element.addEventListener('input', event =>{
+        updateValue(event.target)
+    })
+})
+
+console.log(order_cash)
+
+// 60-64行 == 66行
+// text_number.forEach(element => element.addEventListener('input', event => updateValue(event.target)))
+
+// 60-64行 == 66行 == 68-74行
+// for(var i=0; i<text_number.length; i++){
+//     text_number[i].addEventListener('input', function (event){
+//         updateValue(event.target)
+    
+//     })
+// }
